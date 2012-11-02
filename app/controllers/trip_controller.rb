93341f900142fe8660754c592c3213e7f77cc947
Rory <rorymacqueen@gmail.com>
2012-11-02 12:15:39 -0700

    commit for deploy to heroku

--- app/controllers/trip_controller.rb
+++ app/controllers/trip_controller.rb
@@ -7,6 +7,9 @@ class TripController < ApplicationController
 		@user = User.find_by_id(session["user"])
 		puts @user
 		@all_content = @user.contents
+
+		@all_content = Content.find(:all)
+
 		for content in @all_content
 			content.width = 200
 			content.height = 200
